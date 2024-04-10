34725a8bad59258c24912105f727bed833e9ab50
Zuluaga <zuluagan@corp.nychhc.org>
2024-04-10 16:20:43 -0400

    New THING

--- expy.py
+++ expy.py
@@ -4,3 +4,19 @@ import pandas as pd
 df = pd.read_excel('pyExv1.xlsx')
 
 print(df)
+
+
+'''[ExcelPyv1 f99664e] New THING
+ Committer: Zuluaga <zuluagan@corp.nychhc.org>
+Your name and email address were configured automatically based
+on your username and hostname. Please check that they are accurate.
+You can suppress this message by setting them explicitly:
+
+    git config --global user.name "Your Name"
+    git config --global user.email you@example.com
+
+After doing this, you may fix the identity used for this commit with:
+
+    git commit --amend --reset-author
+
+ 1 file changed, 1 insertion(+)'''
